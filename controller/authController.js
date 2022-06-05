2d892c4105be92a53bf9a0741bb6a43d938c01d0
Mehedi Hasan <mehedi08h@gmail.com>
2022-06-05 16:36:01 +0600

    implement login

--- controller/authController.js
+++ controller/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const { hashPassword, comparePassword } = require("../utils/auth");
 
 exports.register = async (req, res) => {
@@ -29,6 +30,34 @@ exports.register = async (req, res) => {
         await user.save();
         // console.log("saved user", user);
         return res.json({ ok: true });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).send("Error. Try again.");
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        // console.log(req.body);
+        const { email, password } = req.body;
+        // check if our db has user with that email
+        const user = await User.findOne({ email }).exec();
+        if (!user) return res.status(400).send("No user found");
+        // check password
+        const match = await comparePassword(password, user.password);
+        // create signed jwt
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        // return user and token to client, exclude hashed password
+        user.password = undefined;
+        // send token in cookie
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure: true, // only works on https
+        });
+        // send user as json response
+        res.json(user);
     } catch (err) {
         console.log(err);
         return res.status(400).send("Error. Try again.");
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { register } = require("../controller/authController");
+const { register, login } = require("../controller/authController");
 
 // controller
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
