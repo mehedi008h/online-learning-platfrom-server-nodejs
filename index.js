5054403c2ea825ad1a1df5028d906825c8a1f19d
Mehedi Hasan <mehedi08h@gmail.com>
2022-06-05 08:42:44 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+
+// create express app
+const app = express();
+
+// apply middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// route
+app.get("/", (req, res) => {
+    res.send("you hit server endpoint");
+});
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
